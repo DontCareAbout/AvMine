3fed42b329963c6bbe3a0a511b1c80d0a1c7deda
MontyPan <monty.pan@dontcareabout.us>
2020-08-01 11:56:30 +0800

    加入 Controller 取代原本用來阻擋 event 的 div、清理應該用不到的 method

--- src/main/java/us/dontcareabout/avMine/client/component/player/VideoPlayer.java
+++ src/main/java/us/dontcareabout/avMine/client/component/player/VideoPlayer.java
@@ -1,38 +1,24 @@
 package us.dontcareabout.avMine.client.component.player;
 
+import com.google.gwt.dom.client.MediaElement;
 import com.google.gwt.event.dom.client.CanPlayThroughHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.DoubleClickEvent;
-import com.google.gwt.event.dom.client.DoubleClickHandler;
 import com.google.gwt.event.dom.client.LoadedMetadataHandler;
 import com.google.gwt.media.client.Video;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.sencha.gxt.widget.core.client.container.SimpleContainer;
-import com.sencha.gxt.widget.core.client.toolbar.LabelToolItem;
 
 public class VideoPlayer extends SimpleContainer {
 	private final Video v = Video.createIfSupported();
 	private final AbsolutePanel absolute = new AbsolutePanel();
-	private final LabelToolItem blockDiv = new LabelToolItem();
+	private final Controller controller = new Controller(v);
 
 	public VideoPlayer() {
+		bindEvents(v.getMediaElement());
 		v.setControls(false);
 		add(absolute);
 		absolute.add(v, 0, 0);
-		//在 video 上頭蓋一層看不到的 div 來阻攔所有 mouse event
-		absolute.add(blockDiv, 0, 0);
+		absolute.add(controller, 0, 0);
 	}
-
-	//blockDiv 的 DOM event handler
-	public void addClickHandler(ClickHandler handler) {
-		blockDiv.addDomHandler(handler, ClickEvent.getType());
-	}
-
-	public void addDoubleClickHandler(DoubleClickHandler handler) {
-		blockDiv.addDomHandler(handler, DoubleClickEvent.getType());
-	}
-	////
 
 	public void addLoadedMetadataHandler(LoadedMetadataHandler handler) {
 		v.addLoadedMetadataHandler(handler);
@@ -60,8 +46,19 @@ public class VideoPlayer extends SimpleContainer {
 
 	@Override
 	protected void onResize(int width, int height) {
-		super.onResize(width, height);
 		v.setPixelSize(width, height);
-		blockDiv.setPixelSize(width, height);
+		controller.setPixelSize(width, height);
+		super.onResize(width, height);
+	}
+
+	private native void bindEvents(MediaElement mediaElement) /*-{
+		var self = this;
+		mediaElement.addEventListener("timeupdate", function(e) {
+			self.@us.dontcareabout.avMine.client.component.player.VideoPlayer::onTimeupdate()();
+		});
+	}-*/;
+
+	private void onTimeupdate() {
+		controller.setTime(v.getCurrentTime(), v.getDuration());
 	}
 }
